9bb22fada8a788f564874a3edae8374fdb274c22
ABHIRAJ9064 <kabhiraj903@gmail.com>
2024-07-14 07:59:55 +0530

    finished

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,15 @@
-
-import React from "react";
-import './../styles/App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from './Home';
+import UserDetails from './UserDetails';
 
 const App = () => {
   return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+    <Router>
+      <Route exact path="/" component={Home} />
+      <Route path="/user/:id" component={UserDetails} />
+    </Router>
+  );
+};
 
-export default App
+export default App;
