638917e3b05a5b9c8c3648211309d0f740770887
ABHIRAJ9064 <kabhiraj903@gmail.com>
2024-07-16 09:08:19 +0530

    finished

--- src/components/App.js
+++ src/components/App.js
@@ -1,48 +1,15 @@
-import React,{useState,useEffect} from "react";
-import './../styles/App.css';
-import {BrowserRouter as Router,Routes,Route,NavLink} from "react-router-dom"
-import UserDetails from "./UserDetails";
-import Users from "./Users";
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from './Home';
+import UserDetails from './UserDetails';
 
 const App = () => {
-
-  const [users,setUsers] = useState([]);
-  // Fetch data when the component mounts for the first time
-
-  useEffect(()=>{
-
-     fetch("https://jsonplaceholder.typicode.com/users")
-     .then(resp=>resp.json())
-     .then(data=>setUsers(data))
-   
-
-
-  },[])
-
-
   return (
-    <div>
-        {/* Do not remove the main div */}
-        
-       
-      
-        <Router>
-          <Routes>
-            {
-              users.map((user)=>(
-                <>
+    <Router>
+      <Route exact path="/" component={Home} />
+      <Route path="/user/:id" component={UserDetails} />
+    </Router>
+  );
+};
 
-                
-                <Route path="/" element={<UserDetails users ={users} />}/>
-                <Route path="/users/:id" element={<Users users = {users}/>}/>
-                </>
-              ))
-            }
-          </Routes>
-        </Router>
-      
-    </div>
-  )
-}
-
-export default App
+export default App;
